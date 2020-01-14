997b1de7f9e6c6e7747a11fb19154315a8a9184d
eemed <eemed@protonmail.com>
2020-01-14 20:48:33 +0200

    fix error on battery script

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 batt_path=/sys/class/power_supply/BAT1
 
+if ! test -f $batt_path ; then
+    exit 1
+fi
+
 now=$(cat $batt_path/energy_now)
 full=$(cat $batt_path/energy_full)
 percentage=$((100 * $now / $full))
