f25c85126c94ce82b17eaa922a098519bd67d1b3
eemed <eemed@protonmail.com>
2020-04-12 18:09:37 +0300

    alacritty

---
+++ .install-scripts/install-alacritty.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+wget https://github.com/alacritty/alacritty/releases/download/v0.4.2/Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
+sudo apt install ./Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
+rm Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
