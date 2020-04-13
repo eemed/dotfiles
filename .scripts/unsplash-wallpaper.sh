0dad27e73b3b9a324f025c401f6f6de769de6a1e
eemed <eemed@protonmail.com>
2020-04-13 16:58:50 +0300

    better wallpaper switch

--- .scripts/unsplash-wallpaper.sh
+++ .scripts/unsplash-wallpaper.sh
@@ -4,9 +4,12 @@
 #
 # $1 width
 # $2 height
+# $3 search term
 
 width=$1
 height=$2
+search_term="minimal"
+[[ $# -ge 3 ]]  && search_term=$3
 
 url="https://source.unsplash.com/random"
 download_dir="$HOME/.wallpaper"
@@ -15,5 +18,5 @@ wallpaper="${download_dir}/wallpaper"
 test -d $download_dir || mkdir -p $download_dir
 test -f "$wallpaper" && rm "$wallpaper"
 
-wget "${url}/${width}x${height}" -O "$wallpaper"
+wget "${url}/${width}x${height}/?${search_term}" -O "$wallpaper"
 gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper}"
